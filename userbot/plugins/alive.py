e5baf563aac2fbd64c838a0857536f411c021dc0
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-10-18 21:23:26 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,4 +1,4 @@
-# For @TeleBotHelp
+# For @xark047
 """Check if your userbot is working."""
 import os
 import requests
@@ -57,24 +57,24 @@ async def amireallyalive(alive):
     ms = (end - start).microseconds / 1000
     uptime = get_readable_time((time.time() - StartTime))
     if ALV_PIC:
-        tele = f"**Welcome To TeleBot **\n\n"
+        tele = f"**Welcome To Night-wolf **\n\n"
         tele += "`Hey! I'm alive. All systems online and functioning normally!`\n\n"
-        tele += "` 🔸 Telethon version:` **1.16.4**\n` 🔹 Python:` **3.8.3**\n"
-        tele += f"` 🔸 TeleBot Version:` **{telever}**\n"
-        tele += "` 🔹 More Info:` **@TeleBotSupport**\n"
-        tele += f"` 🔸 Sudo :` **{sudo}**\n"
-        tele += f"` 🔹 TeleBot Uptime:` **{uptime}**\n"
-        tele += "` 🔸 Database Status:` **All OK 👌!**\n"
-        tele += f"` 🔹 My pro owner` : **[{DEFAULTUSER}](tg://user?id={myid})**\n\n"
-        tele += "    [✨ GitHub Repository ✨](https://github.com/xditya/TeleBot)"
+        tele += "` ▪️ Telethon version:` **1.16.4**\n` ▪️ Python:` **3.8.3**\n"
+        tele += f"` ▪️ TeleBot Version:` **{telever}**\n"
+        tele += "` ▪️ More Info:` **Ask @xark047**\n"
+        tele += f"` ▪️ Sudo :` **{sudo}**\n"
+        tele += f"` ▪️ Wolf Uptime:` **{uptime}**\n"
+        tele += "` ▪️ Database Status:` **All OK 👌!**\n"
+        tele += f"` ▪️ My owner` : **[{DEFAULTUSER}](tg://user?id={myid})**\n\n"
+        
 
         chat = await alive.get_chat()
         await alive.delete()
         """ For .alive command, check if the bot is running.  """
-        await borg.send_file(alive.chat_id, ALV_PIC,caption=tele, link_preview = False)
+        await borg.send_file(alive.chat_id, ALV_PIC,caption=wolf, link_preview = False)
         await alive.delete()
         return
-    req = requests.get("https://telegra.ph/file/0670190de8e3bddea6d95.png")
+    req = requests.get("https://telegra.ph/file/18a819b4926a0e2d2ce85.png")
     req.raise_for_status()
     file = BytesIO(req.content)
     file.seek(0)
@@ -85,13 +85,13 @@ async def amireallyalive(alive):
         sticker.seek(0)
         await borg.send_message(alive.chat_id, f"**Welcome To TeleBot **\n\n"
                 "`Hey! I'm alive. All systems online and functioning normally!`\n\n"
-                "` 🔸 Telethon version:` **1.16.4**\n` 🔹 Python:` **3.8.3**\n"
-                f"` 🔸 TeleBot Version:` **{telever}**\n"
-                "` 🔹 More Info:` **@TeleBotSupport**\n"
-                f"` 🔸 Sudo :` **{sudo}**\n"
-                f"` 🔹 TeleBot Uptime:` **{uptime}**\n"
-                "` 🔸 Database Status:` **All OK 👌!**\n"
-                f"` 🔹 My pro owner` : **[{DEFAULTUSER}](tg://user?id={myid})**\n\n"
-                "    [✨ GitHub Repository ✨](https://github.com/xditya/TeleBot)", link_preview = False)
+                "` ▪️ Telethon version:` **1.16.4**\n` ▪️ Python:` **3.8.3**\n"
+                f"` ▪️ TeleBot Version:` **{telever}**\n"
+                "` ▪️ More Info:` **@TeleBotSupport**\n"
+                f"` ▪️ Sudo :` **{sudo}**\n"
+                f"` ▪️ TeleBot Uptime:` **{uptime}**\n"
+                "` ▪️ Database Status:` **All OK 👌!**\n"
+                f"` ▪️ My owner` : **[{DEFAULTUSER}](tg://user?id={myid})**\n\n"
+                
         await borg.send_file(alive.chat_id, file=sticker) 
         await alive.delete()
